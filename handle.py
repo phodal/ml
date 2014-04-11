363dee5165ba1609338df90e9cc1df70cab6e211
Phodal <gmszone@qq.com>
2014-04-11 23:02:38 +0800

    add file for month analytics

--- handle.py
+++ handle.py
@@ -22,6 +22,7 @@ def parse_data(jsonfile):
 
     minuteswithcount = [(x, dataarray.count(x)) for x in set(dataarray)]
     f.close()
+    print datacount
     return minuteswithcount
 
 
@@ -33,9 +34,29 @@ def draw_date(files):
         x.append(mwc[0])
         y.append(mwc[1])
 
-    plt.figure(figsize=(8,4))
-    plt.plot(x, y,label = files)
+    plt.figure(figsize=(8, 4))
+    plt.plot(x, y, label=files)
     plt.legend()
     plt.show()
 
-draw_date("data/2014-01-01-0.json")
+# draw_date("data/2014-01-01-0.json")
+parse_data("data/2014-02-01-0.json")
+parse_data("data/2014-02-02-0.json")
+parse_data("data/2014-02-03-0.json")
+parse_data("data/2014-02-04-0.json")
+parse_data("data/2014-02-05-0.json")
+parse_data("data/2014-02-06-0.json")
+parse_data("data/2014-02-07-0.json")
+parse_data("data/2014-02-08-0.json")
+parse_data("data/2014-02-09-0.json")
+parse_data("data/2014-02-10-0.json")
+parse_data("data/2014-02-11-0.json")
+parse_data("data/2014-02-12-0.json")
+parse_data("data/2014-02-13-0.json")
+parse_data("data/2014-02-14-0.json")
+parse_data("data/2014-02-15-0.json")
+parse_data("data/2014-02-16-0.json")
+parse_data("data/2014-02-17-0.json")
+parse_data("data/2014-02-18-0.json")
+parse_data("data/2014-02-19-0.json")
+parse_data("data/2014-02-20-0.json")
