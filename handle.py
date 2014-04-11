2012397dcbcacfbf2fb47e09e8ea8a0082c70d66
Phodal <gmszone@qq.com>
2014-04-12 00:00:07 +0800

    add support of month totals and parse_data

--- handle.py
+++ handle.py
@@ -1,62 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
-import dateutil.parser
 import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+import parse_data as pd
 
 
-def parse_data(jsonfile):
-    f = open(jsonfile, "r")
-    dataarray = []
-    datacount = 0
-
-    for line in open(jsonfile):
-        line = f.readline()
-        lin = json.loads(line)
-        date = dateutil.parser.parse(lin["created_at"])
-        datacount += 1
-        dataarray.append(date.minute)
-
-    minuteswithcount = [(x, dataarray.count(x)) for x in set(dataarray)]
-    f.close()
-    print datacount
-    return minuteswithcount
+def simple_draw(label,y):
+    plt.figure(figsize=(8, 4))
+    plt.plot(y, label=label)
+    plt.legend()
+    plt.show()
 
 
 def draw_date(files):
     x = []
     y = []
-    mwcs = parse_data(files)
+    mwcs = pd.get_minutes_counts_with_id(files)
     for mwc in mwcs:
         x.append(mwc[0])
         y.append(mwc[1])
 
-    plt.figure(figsize=(8, 4))
-    plt.plot(x, y, label=files)
-    plt.legend()
-    plt.show()
+    simple_draw(files, mwcs)
 
-# draw_date("data/2014-01-01-0.json")
-parse_data("data/2014-02-01-0.json")
-parse_data("data/2014-02-02-0.json")
-parse_data("data/2014-02-03-0.json")
-parse_data("data/2014-02-04-0.json")
-parse_data("data/2014-02-05-0.json")
-parse_data("data/2014-02-06-0.json")
-parse_data("data/2014-02-07-0.json")
-parse_data("data/2014-02-08-0.json")
-parse_data("data/2014-02-09-0.json")
-parse_data("data/2014-02-10-0.json")
-parse_data("data/2014-02-11-0.json")
-parse_data("data/2014-02-12-0.json")
-parse_data("data/2014-02-13-0.json")
-parse_data("data/2014-02-14-0.json")
-parse_data("data/2014-02-15-0.json")
-parse_data("data/2014-02-16-0.json")
-parse_data("data/2014-02-17-0.json")
-parse_data("data/2014-02-18-0.json")
-parse_data("data/2014-02-19-0.json")
-parse_data("data/2014-02-20-0.json")
+
+if __name__ == '__main__':
+    results = pd.get_month_total()
+    # results = [6570, 7420, 11274, 12073, 12160, 12378, 12897, 8474, 7984, 12933, 13504, 13763, 13544, 12940, 7119, 7346, 13412, 14008, 12555]
+    simple_draw("month",results)
+    print results
---
+++ parse_data.py
@@ -0,0 +1,47 @@
+__author__ = 'fdhuang'
+__all__ = ["get_minutes_counts_with_id"]
+
+import json
+import dateutil.parser
+
+
+def get_minutes_counts_with_id(jsonfile):
+    datacount,dataarray = handle_json(jsonfile)
+    minuteswithcount = [(x, dataarray.count(x)) for x in set(dataarray)]
+    return minuteswithcount
+
+
+def handle_json(jsonfile):
+    f = open(jsonfile, "r")
+    dataarray = []
+    datacount = 0
+
+    for line in open(jsonfile):
+        line = f.readline()
+        lin = json.loads(line)
+        date = dateutil.parser.parse(lin["created_at"])
+        datacount += 1
+        dataarray.append(date.minute)
+
+    f.close()
+    return datacount, dataarray
+
+
+def get_minutes_count_num(jsonfile):
+    datacount, dataarray = handle_json(jsonfile)
+    return datacount
+
+
+def get_month_total():
+    """
+
+    :rtype : object
+    """
+    monthdaycount = []
+    for i in range(1, 20):
+        if i < 10:
+            filename = 'data/2014-02-0' + i.__str__() + '-0.json'
+        else:
+            filename = 'data/2014-02-' + i.__str__() + '-0.json'
+        monthdaycount.append(get_minutes_count_num(filename))
+    return monthdaycount
